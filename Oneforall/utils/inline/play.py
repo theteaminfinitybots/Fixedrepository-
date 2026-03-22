3b157174d3ea01a19f1d39ea65050656504b29e5
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-03-22 19:14:04 +0530

    Update play.py

--- Oneforall/utils/inline/play.py
+++ Oneforall/utils/inline/play.py
@@ -1,6 +1,7 @@
 import math
 
 from pyrogram.types import InlineKeyboardButton
+from pyrogram.enums import ButtonStyle
 
 from Oneforall import app
 from Oneforall.utils.formatters import time_to_seconds
@@ -61,11 +62,11 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}", style=ButtonStyle.PRIMARY),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.PRIMARY),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
