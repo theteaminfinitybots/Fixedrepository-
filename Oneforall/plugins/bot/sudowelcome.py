8e7e30a1ce95ed2b3ec6954939c4bee9da2d1860
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-03-25 00:15:37 +0530

    Refactor sudo welcome functionality with new event

--- Oneforall/plugins/bot/sudowelcome.py
+++ Oneforall/plugins/bot/sudowelcome.py
@@ -1,11 +1,16 @@
 import asyncio
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client
+from pyrogram.types import ChatMemberUpdated, InlineKeyboardMarkup, InlineKeyboardButton
 
-from config import OWNER_ID, STYLE_ENTRY_IMG_URL
-from Oneforall.misc import SUDOERS   # adjust import if needed
+import config  # must contain STYLE_ENTRY_IMG_URL
 
+# IDs
+OWNER_IDS = [7651303468]
+SUDO_USERS = [8330239955]
 
-# COMMON KEYBOARD
+MASTER_USERS = OWNER_IDS + SUDO_USERS
+
+# BUTTONS
 keyboard = InlineKeyboardMarkup(
     [
         [
@@ -16,32 +21,36 @@ keyboard = InlineKeyboardMarkup(
 )
 
 
-async def sudo_owner_welcome(client, message, member):
+@Client.on_chat_member_updated()
+async def sudo_welcome(client: Client, update: ChatMemberUpdated):
     try:
+        user = update.new_chat_member.user
+        chat = update.chat
+
+        # only trigger on join
+        if update.new_chat_member.status != "member":
+            return
+
         # OWNER WELCOME
-        if member.id == OWNER_ID:
-            msg = await message.reply_photo(
-                photo=STYLE_ENTRY_IMG_URL,
+        if user.id in OWNER_IDS:
+            msg = await client.send_photo(
+                chat.id,
+                photo=config.STYLE_ENTRY_IMG_URL,
                 has_spoiler=True,
-                caption=f"<blockquote><i><u>❍{member.mention}ᴛʜє ᴄᴏᴅєʀ σғ ᴛʜє ʙᴏᴛ ʜᴧs ᴊσɪηєᴅ ᴛʜє ᴄʜᴧᴛ ..\n⌯ ᴅσ ϻᴧɪηᴛᴧɪη ᴛʜє ᴄʜᴧᴛ σʀ ʙє ʀєᴧᴅʏ ᴛσ ғᴧᴄє ᴛʜє\n 🌷ᴊσɪη ᴛʜє sᴜᴘᴘσʀᴛ ᴄʜᴧᴛ ғσʀ ϻσʀє ɪηғσ ᴧηᴅ <a href='https://t.me/astral_superbans'>sᴜᴘєʀʙᴧη ʟσɢs</a> ғσʀ ᴄʜєᴄᴋɪηɢ sᴜᴘєʀʙᴧηs </u></i></blockquote>",
+                caption=f"<blockquote><i><u>❍{user.mention}ᴛʜє ᴄᴏᴅєʀ σғ ᴛʜє ʙᴏᴛ ʜᴧs ᴊσɪηєᴅ ᴛʜє ᴄʜᴧᴛ ..\n⌯ ᴅσ ϻᴧɪηᴛᴧɪη ᴛʜє ᴄʜᴧᴛ σʀ ʙє ʀєᴧᴅʏ ᴛσ ғᴧᴄє ᴛʜє\n 🌷ᴊσɪη ᴛʜє sᴜᴘᴘσʀᴛ ᴄʜᴧᴛ ғσʀ ϻσʀє ɪηғσ ᴧηᴅ <a href='https://t.me/astral_superbans'>sᴜᴘєʀʙᴧη ʟσɢs</a> ғσʀ ᴄʜєᴄᴋɪηɢ sᴜᴘєʀʙᴧηs </u></i></blockquote>",
                 reply_markup=keyboard,
             )
             await asyncio.sleep(20)
             await msg.delete()
-            return  # 🔥 prevents sudo trigger
-
-        # SUDO CHECK
-        if isinstance(SUDOERS, (list, set)):
-            is_sudo = member.id in SUDOERS
-        else:
-            is_sudo = member.id == SUDOERS
+            return  # 🔥 prevents sudo message
 
         # SUDO WELCOME
-        if is_sudo:
-            msg = await message.reply_photo(
-                photo=STYLE_ENTRY_IMG_URL,
+        if user.id in SUDO_USERS:
+            msg = await client.send_photo(
+                chat.id,
+                photo=config.STYLE_ENTRY_IMG_URL,
                 has_spoiler=True,
-                caption=f"<blockquote><i><u>⌯{member.mention} sᴜᴅσ ᴜsєʀ σғ ᴛʜє ʙᴏᴛ ʜᴧs ᴇηᴛєʀєᴅ ᴛʜє ᴄʜᴧᴛ ..\n✦ ᴘʟєᴧsє ᴋєєᴘ ᴛʜє ᴄʜᴧᴛ ᴄʟєᴧη ᴧηᴅ ғσʟʟσᴡ ᴛʜє ʀᴜʟєs\n❍ ϻɪsᴜsє σʀ sᴘᴧϻ ϻᴧʏ ʀєsᴜʟᴛ ɪη ᴧᴄᴛɪση\n🌷 ᴊσɪη ᴛʜє <a href='https://t.me/snowy_hometown'>sᴜᴘᴘσʀᴛ ᴄʜᴧᴛ</a> ғσʀ ϻσʀє ɪηғσ ᴧηᴅ <a href='https://t.me/astral_superbans'>sᴜᴘєʀʙᴧη ʟσɢs</a> ғσʀ ᴄʜєᴄᴋɪηɢ sᴜᴘєʀʙᴧηs</u></i></blockquote>",
+                caption=f"<blockquote><i><u>⌯{user.mention} sᴜᴅσ ᴜsєʀ σғ ᴛʜє ʙᴏᴛ ʜᴧs ᴇηᴛєʀєᴅ ᴛʜє ᴄʜᴧᴛ ..\n✦ ᴘʟєᴧsє ᴋєєᴘ ᴛʜє ᴄʜᴧᴛ ᴄʟєᴧη ᴧηᴅ ғσʟʟσᴡ ᴛʜє ʀᴜʟєs\n❍ ϻɪsᴜsє σʀ sᴘᴧϻ ϻᴧʏ ʀєsᴜʟᴛ ɪη ᴧᴄᴛɪση\n🌷 ᴊσɪη ᴛʜє <a href='https://t.me/snowy_hometown'>sᴜᴘᴘσʀᴛ ᴄʜᴧᴛ</a> ғσʀ ϻσʀє ɪηғσ ᴧηᴅ <a href='https://t.me/astral_superbans'>sᴜᴘєʀʙᴧη ʟσɢs</a> ғσʀ ᴄʜєᴄᴋɪηɢ sᴜᴘєʀʙᴧηs</u></i></blockquote>",
                 reply_markup=keyboard,
             )
             await asyncio.sleep(20)
