20603b60de2eecaa29062dfa79fdaa4146e4136e
˹ 🎧 ᴛᴇᴀᴍ ɪɴғɪɴɪᴛʏ ʙᴏᴛs ˼ <networksmite@gmail.com>
2026-03-24 23:52:09 +0530

    Refactor welcome message handling for owner and sudo

--- Oneforall/plugins/bot/start.py
+++ Oneforall/plugins/bot/start.py
@@ -276,30 +276,42 @@ async def welcome_handler(client, message: Message):
                 await message.stop_propagation()
                 return
 
-            # OWNER WELCOME
-            if member.id == config.OWNER_ID:
-                msg = await message.reply_photo(
-                    photo=config.STYLE_ENTRY_IMG_URL,
-                    caption=f"<blockquote><i><u>❍{member.mention}ᴛʜє ᴄᴏᴅєʀ σғ ᴛʜє ʙᴏᴛ ʜᴧs ᴊσɪηєᴅ ᴛʜє ᴄʜᴧᴛ ..\n⌯ ᴅσ ϻᴧɪηᴛᴧɪη ᴛʜє ᴄʜᴧᴛ σʀ ʙє ʀєᴧᴅʏ ᴛσ ғᴧᴄє ᴛʜє\n 🌷ᴊσɪη ᴛʜє sᴜᴘᴘσʀᴛ ᴄʜᴧᴛ ғσʀ ϻσʀє ɪηғσ ᴧηᴅ <a href='https://t.me/astral_superbans'>sᴜᴘєʀʙᴧη ʟσɢs</a> ғσʀ ᴄʜєᴄᴋɪηɢ sᴜᴘєʀʙᴧηs </u></i></blockquote>",
-                    reply_markup=keyboard,
-                )
-                await asyncio.sleep(20)
-                await msg.delete()
+            keyboard = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("🌷 sᴜᴘᴘσʀᴛ", url="https://t.me/snowy_hometown"),
+            InlineKeyboardButton("🔍 sᴜᴘєʀʙᴧηs", url="https://t.me/astral_superbans"),
+        ]
+    ]
+)
 
-            # SUDO WELCOME
-            if isinstance(SUDOERS, (list, set)):
-                is_sudo = member.id in SUDOERS
-            else:
-                is_sudo = member.id == SUDOERS
+try:
+    # OWNER WELCOME
+    if member.id == config.OWNER_ID:
+        msg = await message.reply_photo(
+            photo=config.STYLE_ENTRY_IMG_URL,
+            has_spoiler=True,
+            caption=f"<blockquote><i><u>❍{member.mention}ᴛʜє ᴄᴏᴅєʀ σғ ᴛʜє ʙᴏᴛ ʜᴧs ᴊσɪηєᴅ ᴛʜє ᴄʜᴧᴛ ..\n⌯ ᴅσ ϻᴧɪηᴛᴧɪη ᴛʜє ᴄʜᴧᴛ σʀ ʙє ʀєᴧᴅʏ ᴛσ ғᴧᴄє ᴛʜє\n 🌷ᴊσɪη ᴛʜє sᴜᴘᴘσʀᴛ ᴄʜᴧᴛ ғσʀ ϻσʀє ɪηғσ ᴧηᴅ <a href='https://t.me/astral_superbans'>sᴜᴘєʀʙᴧη ʟσɢs</a> ғσʀ ᴄʜєᴄᴋɪηɢ sᴜᴘєʀʙᴧηs </u></i></blockquote>",
+            reply_markup=keyboard,
+        )
+        await asyncio.sleep(20)
+        await msg.delete()
 
-            if is_sudo:
-                 msg = await message.reply_photo(
-                    photo=config.STYLE_ENTRY_IMG_URL,
-                    caption=f"<blockquote><i><u>⌯{member.mention} sᴜᴅσ ᴜsєʀ σғ ᴛʜє ʙᴏᴛ ʜᴧs ᴇηᴛєʀєᴅ ᴛʜє ᴄʜᴧᴛ ..\n✦ ᴘʟєᴧsє ᴋєєᴘ ᴛʜє ᴄʜᴧᴛ ᴄʟєᴧη ᴧηᴅ ғσʟʟσᴡ ᴛʜє ʀᴜʟєs\n❍ ϻɪsᴜsє σʀ sᴘᴧϻ ϻᴧʏ ʀєsᴜʟᴛ ɪη ᴧᴄᴛɪση\n🌷 ᴊσɪη ᴛʜє <a href='https://t.me/snowy_hometown'>sᴜᴘᴘσʀᴛ ᴄʜᴧᴛ</a> ғσʀ ϻσʀє ɪηғσ ᴧηᴅ <a href='https://t.me/astral_superbans'>sᴜᴘєʀʙᴧη ʟσɢs</a> ғσʀ ᴄʜєᴄᴋɪηɢ sᴜᴘєʀʙᴧηs</u></i></blockquote>",
-                    reply_markup=keyboard,
-                 )
-                 await asyncio.sleep(20)
-                 await msg.delete()
+    # SUDO WELCOME
+    if isinstance(SUDOERS, (list, set)):
+        is_sudo = member.id in SUDOERS
+    else:
+        is_sudo = member.id == SUDOERS
 
-        except Exception as e:
-            print(f"[WELCOME ERROR] {e}")
+    if is_sudo:
+        msg = await message.reply_photo(
+            photo=config.STYLE_ENTRY_IMG_URL,
+            has_spoiler=True,
+            caption=f"<blockquote><i><u>⌯{member.mention} sᴜᴅσ ᴜsєʀ σғ ᴛʜє ʙᴏᴛ ʜᴧs ᴇηᴛєʀєᴅ ᴛʜє ᴄʜᴧᴛ ..\n✦ ᴘʟєᴧsє ᴋєєᴘ ᴛʜє ᴄʜᴧᴛ ᴄʟєᴧη ᴧηᴅ ғσʟʟσᴡ ᴛʜє ʀᴜʟєs\n❍ ϻɪsᴜsє σʀ sᴘᴧϻ ϻᴧʏ ʀєsᴜʟᴛ ɪη ᴧᴄᴛɪση\n🌷 ᴊσɪη ᴛʜє <a href='https://t.me/snowy_hometown'>sᴜᴘᴘσʀᴛ ᴄʜᴧᴛ</a> ғσʀ ϻσʀє ɪηғσ ᴧηᴅ <a href='https://t.me/astral_superbans'>sᴜᴘєʀʙᴧη ʟσɢs</a> ғσʀ ᴄʜєᴄᴋɪηɢ sᴜᴘєʀʙᴧηs</u></i></blockquote>",
+            reply_markup=keyboard,
+        )
+        await asyncio.sleep(20)
+        await msg.delete()
+
+except Exception as e:
+    print(f"[WELCOME ERROR] {e}")
